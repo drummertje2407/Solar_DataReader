3d7f25029c038d15eb8a5dbc9cff237872db7e34
Wal, van der Fynn (143557) <143557@emelwerda.nl>
2019-03-18 22:33:23 +0100

    Commit1

--- Solar_DataReader/Form1.Designer.cs
+++ Solar_DataReader/Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Btn_Connect = new System.Windows.Forms.Button();
+            this.Console_output = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.configToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeTokenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Btn_Connect
+            // 
+            this.Btn_Connect.Location = new System.Drawing.Point(313, 368);
+            this.Btn_Connect.Name = "Btn_Connect";
+            this.Btn_Connect.Size = new System.Drawing.Size(121, 38);
+            this.Btn_Connect.TabIndex = 0;
+            this.Btn_Connect.Text = "Connect";
+            this.Btn_Connect.UseVisualStyleBackColor = true;
+            this.Btn_Connect.Click += new System.EventHandler(this.Btn_Connect_Click);
+            // 
+            // Console_output
+            // 
+            this.Console_output.BackColor = System.Drawing.Color.Black;
+            this.Console_output.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Console_output.ForeColor = System.Drawing.Color.Green;
+            this.Console_output.Location = new System.Drawing.Point(163, 114);
+            this.Console_output.Name = "Console_output";
+            this.Console_output.ReadOnly = true;
+            this.Console_output.Size = new System.Drawing.Size(489, 185);
+            this.Console_output.TabIndex = 1;
+            this.Console_output.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // configToolStripMenuItem
+            // 
+            this.configToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeTokenToolStripMenuItem});
+            this.configToolStripMenuItem.Name = "configToolStripMenuItem";
+            this.configToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.configToolStripMenuItem.Text = "Config";
+            // 
+            // changeTokenToolStripMenuItem
+            // 
+            this.changeTokenToolStripMenuItem.Name = "changeTokenToolStripMenuItem";
+            this.changeTokenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.changeTokenToolStripMenuItem.Text = "Change Token";
+            this.changeTokenToolStripMenuItem.Click += new System.EventHandler(this.changeTokenToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Console_output);
+            this.Controls.Add(this.Btn_Connect);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Btn_Connect;
+        private System.Windows.Forms.RichTextBox Console_output;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem configToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeTokenToolStripMenuItem;
     }
 }
 
--- Solar_DataReader/Form1.cs
+++ Solar_DataReader/Form1.cs
@@ -1,20 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+using Discord;
+using Discord.WebSocket;
 
 namespace Solar_DataReader
 {
     public partial class Form1 : Form
     {
+        DiscordSocketClient Client;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void Btn_Connect_Click(object sender, EventArgs e)
+        {
+            Client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+
+            Client.Log += Client_Log;
+
+            string Token = "";
+            try
+            {
+                #region GetToken
+
+                using (var Stream = new FileStream(@"C:\Windows\Temp\SolarData\Token.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                using (var Readtoken = new StreamReader(Stream))
+                {
+                    Token = Readtoken.ReadToEnd();
+                }
+
+                #endregion
+            }
+            catch(FileNotFoundException ex) { MessageBox.Show("Error: The token is not yet created of deleted, please make a new one \r\n" + ex.Message.ToString(), "ERROR"); return; }
+            catch (Exception ex) { MessageBox.Show("Error: Cannot acces Token.txt \r\n" + ex.Message.ToString(), "ERROR"); return; }
+            
+
+            try
+            {
+                await Client.LoginAsync(TokenType.Bot, Token);
+                await Client.StartAsync();
+            }
+            catch (Exception ex) { MessageBox.Show("Error: Cannot connect your Bot to the internet \r\n" + ex.Message.ToString(), "ERROR"); return; }
+
+            await Task.Delay(3000);
+
+        }
+
+        private Task Client_Log(LogMessage arg)
+        {
+            Invoke((Action)delegate
+            {
+                Console_output.AppendText(arg + "\n");
+            });
+            return null;
+        }
+
+        private void changeTokenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string Token =  Interaction.InputBox("Type in your token: ", "Input", "Default Text");
+            try { File.WriteAllText(@"C:\Windows\Temp\SolarData\Token.txt", Token); }
+            catch (UnauthorizedAccessException ex) { MessageBox.Show("Error: Cannot acces the Token file \r\n" + ex.Message.ToString(), "ERROR"); return; }
         }
     }
 }
